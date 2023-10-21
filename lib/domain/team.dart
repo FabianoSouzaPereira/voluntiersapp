64f01613e30d6afd7eba5e684aebde2ce1d3a746
Fabiano <fabiano.pereira@seventh.com.br>
2023-10-21 19:24:57 -0300

    add Locale languages

---
+++ lib/domain/team.dart
@@ -0,0 +1,40 @@
+class Team {
+  String id;
+  String name;
+  String description;
+  String type;
+  String leader;
+
+  Team({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.type,
+    required this.leader,
+  });
+
+  factory Team.fromJson(Map<String, dynamic> json) => Team(
+        id: json["id"],
+        name: json["name"],
+        description: json["description"],
+        type: json["type"],
+        leader: json["leader"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "description": description,
+        "type": type,
+        "leader": leader,
+      };
+}
+
+enum Teams {
+  parkingTeam,
+  entranceHallTeam,
+  receptionTeam,
+  hospitalityTeam,
+  announcementteam,
+  otherTeam,
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:voluntiersapp/l10n/l10n.dart';
 import 'package:voluntiersapp/locator.dart';
 import 'package:voluntiersapp/ui/home/home_page.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() async {
   await setupLocator();
@@ -20,6 +22,8 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const HomePage(title: 'Flutter Demo Home Page'),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: L10n.all,
     );
   }
 }
