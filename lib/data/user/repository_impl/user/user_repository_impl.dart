d77ad5e94989154f18896d67dc787bd57eeac42d
Fabiano Pereira <jeovajire2003@gmail.com>
2024-02-23 18:36:09 -0300

    fix imports

--- lib/data/user/repository_impl/user/user_repository_impl.dart
+++ lib/data/user/repository_impl/user/user_repository_impl.dart
@@ -1,4 +1,3 @@
-import 'package:dio/dio.dart';
 import 'package:voluntiersapp/core/http/abstract_http_client.dart';
 import 'package:voluntiersapp/data/models/users_model.dart';
 import 'package:voluntiersapp/domain/repositories/abstractions/abstract_user_repository.dart';
